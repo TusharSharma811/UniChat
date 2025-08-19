0a5a9cba4836f86b47ba61be1f6636d7d61094e6
Tushar Sharma <sharmatushar811@gmail.com>
2025-08-19 21:55:17 +0530

    socket fix

--- backend/src/index.js
+++ backend/src/index.js
@@ -20,7 +20,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "https://uni-chat-ruby.vercel.app",
+    origin: ["https://uni-chat-ruby.vercel.app" , "http://localhost:5173"],
     credentials: true,
   })
 );
